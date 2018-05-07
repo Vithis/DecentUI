0d919482e69f1f41e65f740f9b6048dbe3455e05
Vithis <39049060+Vithis@users.noreply.github.com>
2018-05-07 13:06:06 +0300

    Initial commit

---
+++ DecentUI_WPF/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DecentUI_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public byte transperecys = 75, reds = 50, greens= 50, blues = 50;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StackPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsWindow sets = new SettingsWindow();
+            sets.Show();
+            
+        }
+
+        private void DecentUI_Loaded(object sender, RoutedEventArgs e)
+        {
+            Background = new SolidColorBrush(Color.FromArgb(transperecys, reds, greens, blues));
+        }
+    }
+}
---
+++ DecentUI_WPF/SettingsWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DecentUI_WPF
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MoveSliders();
+
+        }
+
+        private void redSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var MW = Application.Current.Windows.Cast<Window>().FirstOrDefault(window => window is MainWindow) as MainWindow;
+
+            MW.reds = (byte)redSlider.Value;
+
+            redLabel.Content = MW.reds;
+
+            MW.Background = new SolidColorBrush(Color.FromArgb(MW.transperecys, MW.reds, MW.greens, MW.blues)); 
+        }
+
+        private void greenSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var MW = Application.Current.Windows.Cast<Window>().FirstOrDefault(window => window is MainWindow) as MainWindow;
+
+            MW.greens = (byte)greenSlider.Value;
+
+            greenLabel.Content = MW.greens;
+
+            MW.Background = new SolidColorBrush(Color.FromArgb(MW.transperecys, MW.reds, MW.greens, MW.blues));
+        }
+
+        private void blueSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var MW = Application.Current.Windows.Cast<Window>().FirstOrDefault(window => window is MainWindow) as MainWindow;
+
+            MW.blues = (byte)blueSlider.Value;
+
+            blueLabel.Content = MW.blues;
+
+            MW.Background = new SolidColorBrush(Color.FromArgb(MW.transperecys, MW.reds, MW.greens, MW.blues));
+        }
+
+        private void transperencySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var MW = Application.Current.Windows.Cast<Window>().FirstOrDefault(window => window is MainWindow) as MainWindow;
+
+            MW.transperecys = (byte)transperencySlider.Value;
+
+            transperencyLabel.Text = Convert.ToString(MW.transperecys); 
+
+            MW.Background = new SolidColorBrush(Color.FromArgb(MW.transperecys, MW.reds, MW.greens, MW.blues));
+        }
+
+        private void transperencyLabel_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var MW = Application.Current.Windows.Cast<Window>().FirstOrDefault(window => window is MainWindow) as MainWindow;
+
+            MW.transperecys = Convert.ToByte(transperencyLabel.Text);
+
+            MW.Background = new SolidColorBrush(Color.FromArgb(MW.transperecys, MW.reds, MW.greens, MW.blues));
+        }
+
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MoveSliders()
+        {
+            var MW = Application.Current.Windows.Cast<Window>().FirstOrDefault(window => window is MainWindow) as MainWindow;          
+
+            transperencySlider.Value = (int)MW.transperecys;
+            redSlider.Value = (int)MW.reds;
+            greenSlider.Value = (int)MW.greens;
+            blueSlider.Value = (int)MW.blues;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            //MainWindow MW = new MainWindow();
+            double newColor = Math.Round(transperencySlider.Value,0);
+            int newNumber = (int)newColor;
+            //
+            // MW.Background = new SolidColorBrush(Color.FromArgb((byte)newNumber, 0xFF, 0, 0));
+            var targetWindow = Application.Current.Windows.Cast<Window>().FirstOrDefault(window => window is MainWindow) as MainWindow;
+            targetWindow.Background = new SolidColorBrush(Color.FromArgb((byte)newNumber, 0xFF, 0, 0));
+            //labelonhere.Content = (byte)newNumber;
+            
+            
+        }
+
+    }
+}
